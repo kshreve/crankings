b485acc845a844945d5286ae46a0b6d83568c8a0
KShreve <kshreve@gmail.com>
2014-01-19 13:05:36 -0500

    Working with mongolab

--- js/controllers.js
+++ js/controllers.js
@@ -1,15 +1,53 @@
-function HomeCtrl($scope, FootballMongo, $cookies, $location, $http) {
+function HomeCtrl($scope, FootballMongo,BasketballMongo, $cookies, $location, $http) {
+	
 	var teams = {};
 	
 	FootballMongo.all(function(rankings) {
 		$scope.rankings = rankings;
 	});
 	
+		
+	//name, Frank, FBRecord, Brank,BBRecord, Crank
+	function addTeams(teamList, sport) {
+        for(var i =0; i<teamList.length; i++)
+        {
+            var team = teams[teamList[i].TEAM] ||  {"Name":teamList[i].TEAM};
+            switch(sport){
+                case "FB":
+                    team.Frank = teamList[i].RK;
+                    team.FBRecord = teamList[i].RECORD;
+                    team.Crank = team.Frank+50;
+                    break;
+                case "BB":
+                    team.Brank = teamList[i].RK;
+                    team.BBRecord = teamList[i].RECORD;
+                    team.Crank = team.Brank+50;
+                    break;
+                default:
+                    break;
+            }
+            if(team.Brank && team.Frank)
+            {
+                team.Crank = team.Brank + team.Frank;
+            }
+            teams[team.Name] = team;
+        }
+	}
+
 	FootballMongo.getById('Football', 
-		function(out) { 
-			alert(out);
+		function(Teams) {
+            var dateTeams = Teams.DEC0813;
+            addTeams(dateTeams,"FB");
+           
 		}
 	);
+	 
+    BasketballMongo.getById('Basketball', 
+        function(Teams) {
+            var dateTeams = Teams.DEC3013;
+            addTeams(dateTeams,"BB");
+        }
+    );
 	
-	$scope.teams = teams;	
-};
+	$scope.teams = teams;
+}
--- js/services.js
+++ js/services.js
@@ -1,4 +1,7 @@
 angular.module('prankServices',['ngResource'])
 	.factory('FootballMongo',function($mongolabResourceHttp) {
 		return $mongolabResourceHttp('Rankings');
+	})
+	.factory('BasketballMongo',function($mongolabResourceHttp){
+	    return $mongolabResourceHttp('Rankings');
 	});
