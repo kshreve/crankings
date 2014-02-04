d00cde3da0cdc2515c9c41c2fdb37236d3ef7bba
KShreve <kshreve@gmail.com>
2014-02-03 20:26:48 -0500

    a

--- server.js
+++ server.js
@@ -1,17 +1,4 @@
-var app = require('http').createServer(handler)
-, fs = require('fs')
-
-app.listen(process.env.PORT || 8001);
-
-function handler (req, res) {
-    fs.readFile('index.html',
-    function (err, data) {
-        if (err) {
-            res.writeHead(500);
-            return res.end('Error loading index.html');
-        }
-        
-        res.writeHead(200, {'Content-Type': 'text/html', "Content-Length": data.length});
-        res.end(data);
-    });
-}
+var connect = require('connect');
+connect.createServer(
+    connect.static(__dirname)
+).listen(8080);
