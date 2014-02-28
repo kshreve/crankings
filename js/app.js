903e937849c22bb47458a2d37fca903e6f90c486
KShreve <kshreve@gmail.com>
2014-02-28 15:41:08 -0500

    changing it to be more of my whole website instead of just crankings

--- js/app.js
+++ js/app.js
@@ -1,8 +1,8 @@
 angular.module('prank', ['prankServices','ngCookies','prankFilters','mongolabResourceHttp']).
     config(['$routeProvider', function($routeProvider) {
         $routeProvider.
-            when('/', {templateUrl: 'partials/home.html', controller: HomeCtrl})
-            .when('/xkcd',{templateUrl: 'partials/now.html', controller: XkcdCtrl})
+            when('/crankings', {templateUrl: 'partials/crankings.html', controller: HomeCtrl})
+            .when('/xkcd', {templateUrl: 'partials/now.html', controller: XkcdCtrl})
             .otherwise({redirectTo: '/'});
     }])
 	.constant('MONGOLAB_CONFIG',{API_KEY:'FmIllpfosmgRaRMQtSEVwizeCBTLl2w1', DB_NAME:'prankings'});;
